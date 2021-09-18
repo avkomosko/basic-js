e531d3ef4dd9ccf9a135f82e565009353f7c4195
Aliaksei <avkomosko@gmail.com>
2021-09-18 17:26:10 +0300

    feat: implement what season function

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,31 +2,28 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
 export default function createDreamTeam(members) {
   if (Array.isArray(members)) {
-    let arr =[];
-  for (let name of members) {
-    if (typeof name === 'string') {
-      name = name.trim();
-      arr.push(name[0].toUpperCase());
+    let arr = [];
+    for (let name of members) {
+      if (typeof name === 'string') {
+        name = name.trim();
+        arr.push(name[0].toUpperCase());
+      }
     }
-    
-  }
-  return arr.sort().join('');
+    return arr.sort().join('');
   } else {
     return false;
   }
-  
-  // remove line with error and write your code here
 }
