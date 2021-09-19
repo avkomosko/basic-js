4420d8b1f097356a1720673ec4ceb9f700f65588
Aliaksei <avkomosko@gmail.com>
2021-09-19 22:33:44 +0300

    feat implement base functionality of extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,20 +2,56 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let result = '';
+  let separator = options.separator || '+';
+  let inputAddition = '';
+  inputAddition += options.addition;
+  let resultAddition = '';
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator || '|';
+  if (additionRepeatTimes > 1) {
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      if (j < additionRepeatTimes - 1) {
+        resultAddition += `${inputAddition}` + `${additionSeparator}`;
+      } else if (j === additionRepeatTimes - 1) {
+        resultAddition += `${inputAddition}`;
+      }
+      console.log(inputAddition, resultAddition);
+    }
+    console.log(resultAddition);
+  } else {
+    resultAddition += `${inputAddition}`;
+  }
+  
+  if (!options.repeatTimes || options.repeatTimes === 1) {
+    if (!inputAddition || !resultAddition) {
+      return str;
+    } else {
+      result += str + `${resultAddition}`;
+    }
+  } else if (options.repeatTimes > 1) {
+      for (let i = 0; i < options.repeatTimes; i++) {
+      if (i < options.repeatTimes - 1) {
+        result += str + `${resultAddition}` + `${separator}`;
+      } else if (i === options.repeatTimes - 1) {
+        result += str + `${resultAddition}`;
+      }
+    }
+  }
+  
+  return result;
 }
